efa613d3a91f6cdfaef8150d3dea6c2cf370fa23
4goodapp <4goodapp@gmail.com>
2021-06-19 14:34:50 +0000

    Fix expired SSL certificate Error

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -18,11 +18,13 @@ module Spree
     end
 
     def provider
-      ::PayPal::SDK.configure(
-        :mode      => preferred_server.present? ? preferred_server : "sandbox",
-        :username  => preferred_login,
-        :password  => preferred_password,
-        :signature => preferred_signature)
+      ::PayPal::SDK.configure(     
+        mode:      preferred_server.present? ? preferred_server : "sandbox",
+        username:  preferred_login,
+        password:  preferred_password,
+        signature: preferred_signature,
+        ssl_options: { ca_file: nil }
+      )
       provider_class.new
     end
 
